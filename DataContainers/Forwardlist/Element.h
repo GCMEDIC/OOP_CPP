3af9e0a194c405320c1350f545c5a5a65b08de7c
Miha_dr72@itstepru.onmicrosoft.com <Miha_dr72@itstepru.onmicrosoft.com>
2021-05-26 20:41:54 +0300

    Templated ForwardList separated

---
+++ DataContainers/Forwardlist/Element.h
@@ -0,0 +1,30 @@
+#pragma once
+#pragma warning(disable:4326)
+#include<iostream>
+#include<string>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define delimiter "\n--------------------------------------------------------\n"
+
+
+template<typename T>class Iterator;
+template<typename T>class ForwardList;
+
+template<typename T>
+class Element
+{
+	T Data;		//«начение элемента
+	Element* pNext;	//Pointer to next - ”казатель на следующий элемент
+	static int count;
+public:
+	const Element* getpNext()const;
+	T getData()const;
+	Element(T Data, Element* pNext = nullptr);
+	~Element();
+	friend class Iterator<T>;
+	friend class ForwardList<T>;
+	friend ForwardList<T> operator+(const ForwardList<T>& left, const ForwardList<T>& right);
+};
---
+++ DataContainers/Forwardlist/ForwardList.h
@@ -0,0 +1,32 @@
+#pragma once
+#include"iterator.h"
+
+template<typename T>
+class ForwardList
+{
+	unsigned int size;
+	Element<T>* Head;	//”казатель на начальный элемент списка
+public:
+	unsigned int get_size()const;
+	const Element<T>* getHead()const;
+	Iterator<T> begin();
+	Iterator<T> end();
+	ForwardList();
+	ForwardList(const std::initializer_list<T> il);
+	ForwardList(const ForwardList<T>& other);
+	ForwardList(ForwardList&& other) noexcept;
+	~ForwardList();
+	//          Operators:
+	ForwardList& operator =(const ForwardList<T>& other);
+	ForwardList<T>& operator=(ForwardList<T>&& other)noexcept;
+	T& operator[](int index);
+	//			Adding elements:
+	void push_front(T Data);
+	void push_back(T Data);
+	void insert(int Index, T Data);
+	//			Removing elements:
+	void pop_front();
+	void pop_back();
+	//			Methods:
+	void print()const;
+};
